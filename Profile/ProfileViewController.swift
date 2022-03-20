89fa6d1f517c7f4431106c7cc7371edf2b37a7ab
Mikhail Usachev <engineer.asutp@mail.ru>
2022-03-20 21:43:26 +0300

    Всё на frame

--- Profile/ProfileViewController.swift
+++ Profile/ProfileViewController.swift
@@ -9,24 +9,18 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
-    let myProfileHeaderView = ProfileHeaderView()//(frame: CGRect(x: 0, y: 0, width: 375, height: 367))
+    let myProfileHeaderView = ProfileHeaderView(frame: CGRect(x: 0, y: 91, width: 390, height: 367))
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        myProfileHeaderView.backgroundColor = UIColor.lightGray
-        myProfileHeaderView.translatesAutoresizingMaskIntoConstraints = false
 
+        myProfileHeaderView.backgroundColor = UIColor.lightGray
+        myProfileHeaderView.autoresizingMask = [.flexibleRightMargin, .flexibleLeftMargin, .flexibleWidth]
         view.addSubview(myProfileHeaderView)
 
-        NSLayoutConstraint.activate([
-            myProfileHeaderView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 0),
-            myProfileHeaderView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: 0),
-            myProfileHeaderView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            myProfileHeaderView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
-        ])
     }
     override func viewWillLayoutSubviews() {
-        myProfileHeaderView.frame = view.frame// = CGRect(x: 0, y: 64, width: 375, height: 367)
+
     }
 
     /*
